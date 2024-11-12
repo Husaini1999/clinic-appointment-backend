acf4cff1c423c43edc6a019fc8a1b377c046d10c
Husaini1999 <husainimuhd99@yahoo.com>
2024-11-13 02:29:16 +0800

    updated config

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '127.0.0.1', () => {
-	console.log(`Server is running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Server is running on port ${PORT}`);
 	console.log('Environment:', process.env.NODE_ENV);
 	console.log('MongoDB URI:', process.env.MONGODB_URI);
 });
